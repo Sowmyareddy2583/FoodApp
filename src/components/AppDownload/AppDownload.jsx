990378ede51d64533e9517ada2952b9fb33bd9e8
Sowmyareddy2583 <127490161+Sowmyareddy2583@users.noreply.github.com>
2024-06-10 17:16:35 +0530

    responsiveness implemented

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better experience download <br />
+        Tomato APP
+      </p>
+      <div className="platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
